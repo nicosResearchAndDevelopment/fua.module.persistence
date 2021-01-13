2e2ff7c0f55fc7b7e6e505eb23e6adb570940864
Simon Petrac <pfoerdie@gmail.com>
2021-01-13 15:22:34 +0100

    renamed variable

--- test/DataFactoryInterface.test.js
+++ test/DataFactoryInterface.test.js
@@ -1,56 +1,56 @@
 const
 	{ describe, it } = require('mocha'),
 	expect = require('expect'),
-	DataFactory = require('../src/module.persistence.js');
+	dataFactory = require('../src/module.persistence.js');
 
 describe('module.persistence : DataFactoryInterface', function() {
 
 	it('should have a function "namedNode" returning a NamedNode', function() {
-		expect(typeof DataFactory.namedNode).toBe('function');
-		expect(DataFactory.namedNode('http://example.org/test')).toMatchObject({
+		expect(typeof dataFactory.namedNode).toBe('function');
+		expect(dataFactory.namedNode('http://example.org/test')).toMatchObject({
 			termType: 'NamedNode',
 			value: 'http://example.org/test'
 		});
 	});
 
 	it('should have a function "blankNode" returning a BlankNode', function() {
-		expect(typeof DataFactory.blankNode).toBe('function');
-		expect(DataFactory.blankNode('1')).toMatchObject({
+		expect(typeof dataFactory.blankNode).toBe('function');
+		expect(dataFactory.blankNode('1')).toMatchObject({
 			termType: 'BlankNode',
 			value: '1'
 		});
 	});
 
 	it('should have a function "literal" returning a Literal', function() {
-		expect(typeof DataFactory.literal).toBe('function');
-		expect(DataFactory.literal('hello world')).toMatchObject({
+		expect(typeof dataFactory.literal).toBe('function');
+		expect(dataFactory.literal('hello world')).toMatchObject({
 			termType: 'Literal',
 			value: 'hello world'
 		});
 	});
 
 	it('should have a function "variable" returning a Variable', function() {
-		expect(typeof DataFactory.variable).toBe('function');
-		expect(DataFactory.variable('test')).toMatchObject({
+		expect(typeof dataFactory.variable).toBe('function');
+		expect(dataFactory.variable('test')).toMatchObject({
 			termType: 'Variable',
 			value: 'test'
 		});
 	});
 
 	it('should have a function "defaultGraph" returning a DefaultGraph', function() {
-		expect(typeof DataFactory.defaultGraph).toBe('function');
-		expect(DataFactory.defaultGraph()).toMatchObject({
+		expect(typeof dataFactory.defaultGraph).toBe('function');
+		expect(dataFactory.defaultGraph()).toMatchObject({
 			termType: 'DefaultGraph'
 		});
 	});
 
 	it('should have a function "quad" returning a Quad', function() {
-		expect(typeof DataFactory.quad).toBe('function');
-		expect(DataFactory.quad(
-			DataFactory.blankNode('1'),
-			DataFactory.namedNode('http://example.org/test'),
-			DataFactory.literal('hello world'),
-			DataFactory.defaultGraph()
+		expect(typeof dataFactory.quad).toBe('function');
+		expect(dataFactory.quad(
+			dataFactory.blankNode('1'),
+			dataFactory.namedNode('http://example.org/test'),
+			dataFactory.literal('hello world'),
+			dataFactory.defaultGraph()
 		)).toMatchObject({
 			termType: 'Quad',
 			subject: {
