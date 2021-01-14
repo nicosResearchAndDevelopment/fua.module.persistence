80547eb3ac04f66b4f59c96e4c75d079d361f9b2
Simon Petrac <pfoerdie@gmail.com>
2021-01-14 18:03:00 +0100

    splitted tests into DataCore and Data

--- test/DataFactoryInterface.test.js
+++ test/DataFactoryInterface.test.js
@@ -5,70 +5,10 @@ const
 
 describe('module.persistence : DataFactoryInterface', function() {
 
-	it('should have a function "namedNode" returning a NamedNode', function() {
-		expect(typeof dataFactory.namedNode).toBe('function');
-		expect(dataFactory.namedNode('http://example.org/test')).toMatchObject({
-			termType: 'NamedNode',
-			value: 'http://example.org/test'
-		});
-	});
-
-	it('should have a function "blankNode" returning a BlankNode', function() {
-		expect(typeof dataFactory.blankNode).toBe('function');
-		expect(dataFactory.blankNode('1')).toMatchObject({
-			termType: 'BlankNode',
-			value: '1'
-		});
-	});
-
-	it('should have a function "literal" returning a Literal', function() {
-		expect(typeof dataFactory.literal).toBe('function');
-		expect(dataFactory.literal('hello world')).toMatchObject({
-			termType: 'Literal',
-			value: 'hello world'
-		});
-	});
-
-	it('should have a function "variable" returning a Variable', function() {
-		expect(typeof dataFactory.variable).toBe('function');
-		expect(dataFactory.variable('test')).toMatchObject({
-			termType: 'Variable',
-			value: 'test'
-		});
-	});
-
-	it('should have a function "defaultGraph" returning a DefaultGraph', function() {
-		expect(typeof dataFactory.defaultGraph).toBe('function');
-		expect(dataFactory.defaultGraph()).toMatchObject({
-			termType: 'DefaultGraph'
-		});
-	});
-
-	it('should have a function "quad" returning a Quad', function() {
-		expect(typeof dataFactory.quad).toBe('function');
-		expect(dataFactory.quad(
-			dataFactory.blankNode('1'),
-			dataFactory.namedNode('http://example.org/test'),
-			dataFactory.literal('hello world'),
-			dataFactory.defaultGraph()
-		)).toMatchObject({
-			termType: 'Quad',
-			subject: {
-				termType: 'BlankNode',
-				value: '1'
-			},
-			predicate: {
-				termType: 'NamedNode',
-				value: 'http://example.org/test'
-			},
-			object: {
-				termType: 'Literal',
-				value: 'hello world'
-			},
-			graph: {
-				termType: 'DefaultGraph'
-			}
-		});
+	it('should have a function "isNamedNode" returning if argument is a NamedNode', function() {
+		expect(typeof dataFactory.isNamedNode).toBe('function');
+		expect(dataFactory.isNamedNode(dataFactory.namedNode('http://example.org/test'))).toBeTruthy();
+		expect(dataFactory.isNamedNode(dataFactory.blankNode('1'))).toBeFalsy();
 	});
 
 	// TODO more tests
