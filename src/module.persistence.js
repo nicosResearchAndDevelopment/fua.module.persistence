a9e7fccdbd8167ae975ee0979e38c9d7a55b780f
Simon Petrac <pfoerdie@gmail.com>
2021-03-03 10:19:10 +0100

    wrapFactory convenience method for other implementations

--- src/module.persistence.js
+++ src/module.persistence.js
@@ -1,5 +1,18 @@
-exports.TermFactory = require('./module.persistence.TermFactory.js');
-exports.DataFactory = require('./module.persistence.DataFactory.js');
-exports.Dataset     = require('./module.persistence.Dataset.js');
-exports.DataStore   = require('./module.persistence.DataStore.js');
-Object.freeze(exports);
+const persistence = exports;
+
+persistence.TermFactory = require('./module.persistence.TermFactory.js');
+persistence.DataFactory = require('./module.persistence.DataFactory.js');
+persistence.Dataset     = require('./module.persistence.Dataset.js');
+persistence.DataStore   = require('./module.persistence.DataStore.js');
+
+persistence.wrapFactory = (factory) => ({
+    namedNode:    (iri) => factory.namedNode(iri),
+    blankNode:    (id) => factory.blankNode(id),
+    literal:      (value, langOrDt) => factory.literal(value, langOrDt),
+    variable:     (name) => factory.variable(name),
+    defaultGraph: () => factory.defaultGraph(),
+    quad:         (subj, pred, obj, graph) => factory.quad(subj, pred, obj, graph),
+    dataset:      (quads) => new persistence.Dataset(quads, factory)
+});
+
+Object.freeze(persistence);
