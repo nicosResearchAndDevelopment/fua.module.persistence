e225b157368311aa0aa85cc6d78c56a977c80e5c
Simon Petrac <pfoerdie@gmail.com>
2021-03-08 14:51:43 +0100

    removed wrapFactory from module.persistence

--- src/module.persistence.js
+++ src/module.persistence.js
@@ -5,16 +5,4 @@ persistence.DataFactory = require('./module.persistence.DataFactory.js');
 persistence.Dataset     = require('./module.persistence.Dataset.js');
 persistence.DataStore   = require('./module.persistence.DataStore.js');
 
-persistence.wrapFactory = (factory) => ({
-    namedNode:    (iri) => factory.namedNode(iri),
-    blankNode:    (id) => factory.blankNode(id),
-    literal:      (value, langOrDt) => factory.literal(value, langOrDt),
-    variable:     (name) => factory.variable(name),
-    defaultGraph: () => factory.defaultGraph(),
-    quad:         (subj, pred, obj, graph) => factory.quad(subj, pred, obj, graph),
-    fromTerm:     (orig) => factory.fromTerm(orig),
-    fromQuad:     (orig) => factory.fromQuad(orig),
-    dataset:      (quads) => new persistence.Dataset(quads, factory)
-});
-
 Object.freeze(persistence);
